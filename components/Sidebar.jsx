d8f1ee3c702624555142735929665118d86bc284
Hemansh <hemansh.bhatia@infrar3d.ca>
2024-04-30 14:22:24 -0400

    sign-out functionality added

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,12 +1,25 @@
+'use client'
+
+import { signOut, useSession } from "next-auth/react";
 import NewChat from "./newchat";
 
 const Sidebar = () => {
+  const { data: session } = useSession();
+
   return (
-    <div className="p-2 flex flex-col h-screen">
-        Sidebar here
+    <div className="p-2 flex flex-col h-screen justify-between">
         <div>
             <NewChat />
         </div>
+
+        {
+          session && session.user && 
+          <img src={session.user.image}
+            onClick={() => signOut()}
+            alt="Profile pic" 
+            className="h-12 w-12 rounded-full cursor-pointer mx-auto mb-2 hover:opacity-50"
+          />
+        }
     </div>
   )
 }
