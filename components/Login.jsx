02252e76d165704bbe3ff08765e07d4a0fa2a002
Hemansh <hemansh.bhatia@infrar3d.ca>
2024-05-28 11:33:00 -0400

    Login page design change

--- components/Login.jsx
+++ components/Login.jsx
@@ -2,20 +2,17 @@
 
 import { signIn } from "next-auth/react";
 import Image from "next/image";
-import logo from "../public/vercel.svg";
+import logo from "../public/logo.jpg";
 
 const Login = () => {
   return (
-    <div className="bg-[#333333] h-screen flex flex-col justify-center items-center">
-        <Image 
-            src={logo}
-            width={300}
-            height={300}
-            alt="logo"
-            className=""
-        />
-        <button onClick={() => signIn('google')} className="mt-8 text-2xl font-bold animate-pulse">
-            Sign in to DumbAI
+    <div className="bg-teal-700 h-screen flex flex-col justify-center items-center">
+        <h1 className="text-5xl font-bold flex">
+          <Image src={logo} alt="Logo for" className="rounded-full w-14 mr-5" />
+          DumbAI
+        </h1>
+        <button onClick={() => signIn('google')} className="mt-5 text-2xl bg-slate-700/[0.6] animate-pulse duration-200 hover:bg-gray-100 hover:text-black w-fit px-10 py-2 rounded-full">
+            Sign in
         </button>
     </div>
   )
